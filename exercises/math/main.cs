6e044c5f1c2ff5be9c535d9f95fa42546b7e499c
Cradock <mcosterlund@gmail.com>
2022-02-09 11:37:10 +0100

    Adds the Math exercise

---
+++ exercises/math/main.cs
@@ -0,0 +1,8 @@
+using static System.Console;
+using static System.Math;
+public static class math{
+	public static void Main(){
+		double sqrt2=Sqrt(2.0), exppi=Pow(PI,E), piexp=Pow(E,PI);
+		Write($"sqrt2={sqrt2} exppi={exppi} piexp={piexp}\n");
+	}
+}
