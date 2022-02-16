fbf541eeca8b33832ccf9791d0963b3a10173f3d
Cradock <mcosterlund@gmail.com>
2022-02-16 09:43:33 +0100

    Added notes to lecture 5, though things are incomplte

---
+++ exercises/epsilon/epsilon.cs
@@ -0,0 +1,19 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class minmax{
+	static void Main(){
+		int i=1;
+		while(i+1>i){i++;}
+		WriteLine($"my max int={i}");
+
+		while(i-1<i){i++;}
+		WriteLine($"my min int={i}");
+
+	}
+}
+
+class epsilon{
+	
+}
---
+++ lectures/Lecture5_InputOutput/stdin.cs
@@ -0,0 +1,13 @@
+class main{
+	static public int Main(){
+	System.Console.WriteLine("this goes to stdout");
+	System.Console.Out.WriteLine("this goes to stdout");
+	System.Console.Error.WriteLine("this goes to stderr");
+	System.IO.TextWriter stdout=System.Console.Out;
+
+	string line=System.Console.ReadLine();
+	WriteLine(line);
+
+	return 0;
+	}
+}
