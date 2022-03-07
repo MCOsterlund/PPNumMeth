5df67a03d193017015915f2f674b6c1f8ea3ea42
Cradock <mcosterlund@gmail.com>
2022-03-07 17:58:19 +0100

    Added exercise integration

---
+++ exercises/integration/main.cs
@@ -0,0 +1,23 @@
+using System;
+using static System.Math;
+using static System.Console;
+
+class main{
+
+public static double erf(double z){
+	Func<double,double> f=delegate(double x){return Exp(-x*x);};
+	double result = integrate.quad(f:f,a:0,b:z,acc:1e-6,eps:0);
+	return result*2/Sqrt(PI);
+}
+
+public static void Main(){
+	int ncalls=0;
+	Func<double,double> f = delegate(double x){
+		ncalls++;
+		return Log(x)/Sqrt(x);};
+	double result = integrate.quad(f:f,a:0,b:1,acc:1e-6,eps:0);
+	WriteLine($"result={result}\n, ncalls={ncalls}\n");
+	for(double x=-3;x<=3;x+=1.0/8)
+		Error.WriteLine($"{x} {erf(x)}");
+}
+}
