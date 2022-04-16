d765d5cd5ea97581cfee59da26ede3647eece4d1
Cradock <mcosterlund@gmail.com>
2022-04-16 11:28:49 +0200

    Added part A of quad homework

---
+++ homeworks/quadratures/PartA/main.cs
@@ -0,0 +1,36 @@
+using System;
+using static System.Console;
+using static System.Math;
+using System.IO;
+
+class main{
+    public static void Main(){
+        double a=0, b=1;
+        Func<double,double> fs1=delegate(double x){return Sqrt(x);};
+        Func<double,double> fs2=delegate(double x){return 1.0/Sqrt(x);};
+        Func<double,double> fs3=delegate(double x){return 4*Sqrt(1-x*x);};
+        Func<double,double> fs4=delegate(double x){return Log(x)/Sqrt(x);};
+
+        //Calculating the integrals
+        double Q1=integrate.quad(fs1,a,b);
+        double Q2=integrate.quad(fs2,a,b);
+        double Q3=integrate.quad(fs3,a,b);
+        double Q4=integrate.quad(fs4,a,b);
+
+        WriteLine("Intgrals from 0 to 1 of various functions: \n");
+        WriteLine($"Sqrt(x)={Q1} \n");
+        WriteLine($"1/Sqrt(x)={Q2} \n");
+        WriteLine($"4*Sqrt(1-x^2)={Q3} \n");
+        WriteLine($"Log(x)/Sqrt(x)={Q4} \n");
+
+        //Error function. Included in the quad routine.
+        var erfnum=new StreamWriter("erf_calc.txt");
+        for(double z=0; z<=3.5; z+=0.1){
+            double erf_quad=integrate.erf(z);
+            erfnum.WriteLine($"{z} {erf_quad}");
+        }
+        erfnum.Close();
+
+
+    }//Main
+}//main
---
+++ homeworks/quadratures/PartA/quad.cs
@@ -0,0 +1,31 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class integrate{
+    public static double quad(
+        Func<double,double> f, 
+        double a,
+        double b,
+        double delt=0.001,
+        double eps=0.001,
+        double f2=double.NaN,
+        double f3=double.NaN
+    ){
+        double h=b-a;
+        if(double.IsNaN(f2)){f2=f(a+2*h/6); f3=f(a+4*h/6);} //First function call
+        double f1=f(a+h/6), f4=f(a+5*h/6);
+        double Q= (2*f1+f2+f3+2*f4)/6*(b-a); //Higer order rule
+        double q= (f1+f2+f3+f4)/4*(b-a); //Lower order rule
+        double err=Abs(Q-q);
+        if(err<= delt+eps*Abs(Q)) return Q;
+        else return quad(f,a,(a+b)/2,delt/Sqrt(2),eps,f1,f2)+
+                    quad(f,(a+b)/2,b,delt/Sqrt(2),eps,f3,f4);
+    }//quad
+
+    public static double erf(double z){
+        Func<double,double> f_erf=delegate(double x){return 2/Sqrt(PI)*Exp(-x*x);};
+        double erf_int=quad(f_erf,0,z);
+        return erf_int;
+    }
+}//integrate
