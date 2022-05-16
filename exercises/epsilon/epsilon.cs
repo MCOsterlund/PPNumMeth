b94c2d91469462fdf6807ced614ed0e7d993e541
Cradock <mcosterlund@gmail.com>
2022-05-16 14:35:17 +0200

    Cleaned up epsilon.cs

--- exercises/epsilon/epsilon.cs
+++ exercises/epsilon/epsilon.cs
@@ -5,16 +5,18 @@ using static System.Math;
 static class main{
 
 	static void minmax(){
+			WriteLine("Maximum/minimum representable integers.");
 			int i=1;
 			while(i+1>i){i++;}
 			WriteLine($"my max int={i}");
 
 			while(i-1<i){i++;}
-			WriteLine($"my min int={i}");
+			WriteLine($"my min int={i}\n");
 
 		}
 
 	static void epsilon(){
+		WriteLine("Machine epsilon");
 		double x=1;
        		while(1+x!=1){x/=2;}
        		x*=2;
@@ -24,9 +26,10 @@ static class main{
        		y*=2F;	
 
 		Console.WriteLine($"double precision: {x}.Pow(2,-52)={System.Math.Pow(2,-52)}");
-		Console.WriteLine($"float precision: {x}.Pow(2,-23)={System.Math.Pow(2,-23)}");
+		Console.WriteLine($"float precision: {x}.Pow(2,-23)={System.Math.Pow(2,-23)}\n");
 		}
 	static void tiny(){
+		WriteLine($"Comparing '1-tiny' and 'tiny-1'.");
 		int n=(int) 1e6;
 		double epsilon = Pow(2,-52);
 		double tiny=epsilon/2;
@@ -38,7 +41,7 @@ static class main{
 
 		for(int i=0; i<n; i++){sumB+=tiny;} 
 		sumB+=1;
-		WriteLine($"sumB-1={sumB-1:e} should be {n*tiny:e}");
+		WriteLine($"sumB-1={sumB-1:e} should be {n*tiny:e}\n");
 	}
 	static bool approx(double a, double b, double tau=1e-9, double epsilon=1e-9){
 		return (Abs(a-b)<tau || Abs(a-b)/(Abs(a)+Abs(b))<epsilon);
